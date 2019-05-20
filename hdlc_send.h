e210dd80db3cdf04c121e2e18ffefbbd6a523696
svusak <svusak@ttu.ee>
2019-05-20 21:49:30 +0300

    added special header to ax25 frames

--- hdlc_send.h
+++ hdlc_send.h
@@ -1,10 +1,12 @@
 
+
 /* hdlc_send.h */
 
 int hdlc_send_frame (int chan, unsigned char *fbuf, int flen, int bad_fcs);
 
 int hdlc_send_flags (int chan, int flags, int finish);
 
-/* end hdlc_send.h */
+int hdlc_send_header (int chan, unsigned  char *fbuf, int flen);
 
+/* end hdlc_send.h */
 
